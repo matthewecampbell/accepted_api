379507eef1e6702b87f7e73263d8e6f9d6830539
Matthew Campbell <ematthewcampbell@gmail.com>
2016-09-19 21:28:58 -0600

    fixed schema

--- db/migrate/20160919214239_add_act_low_to_colleges.rb
+++ db/migrate/20160919214239_add_act_low_to_colleges.rb
@@ -1,6 +1,23 @@
 class AddActLowToColleges < ActiveRecord::Migration[5.0]
   def change
-    add_column :colleges, :act_low, :integer
-    add_column :colleges, :act_high, :integer
+    create_table :colleges do |t|
+      t.string :name
+      t.integer :total_price_in_state
+      t.integer :total_price_out_of_state
+      t.integer :percent_admitted
+      t.integer :yield
+      t.integer :total_applicants
+      t.string :act_range
+      t.string :city
+      t.string :longitude
+      t.string :latitude
+      t.string :affiliation
+      t.integer :six_year_grad_rate
+      t.integer :total_enrollment
+      t.timestamp :updated_at
+      t.timestamp :created_at
+      t.integer :act_low
+      t.integer :act_high
+    end
   end
 end
--- db/migrate/20160919214314_remove_table_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTableUsers < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :users
-  end
-end
