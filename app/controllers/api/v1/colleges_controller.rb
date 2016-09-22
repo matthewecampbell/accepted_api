e0c920ab512caffa3136ce9b81621d68d383ec1e
Matthew Campbell <ematthewcampbell@gmail.com>
2016-09-22 01:00:25 -0600

    authentication works

--- app/controllers/api/v1/colleges_controller.rb
+++ app/controllers/api/v1/colleges_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::CollegesController < Api::V1::BaseController
-  skip_before_filter :verify_authenticity_token
-  
   def index
     @colleges = Rails.cache.fetch("colleges") {College.all}
 
